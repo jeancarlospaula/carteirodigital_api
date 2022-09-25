45712338a383f51f09577af0ab86504a71516fd6
jean.depaula <jean.depaula@obraprimaweb.com.br>
2022-09-25 13:15:31 -0300

    chore: removed empty field of api return

--- src/lib/getEventAddressData.ts
+++ src/lib/getEventAddressData.ts
@@ -2,9 +2,9 @@ import { IEventAddress } from '../interfaces/ITrackEvents'
 
 const getEventAddressData = (address: string = ''): IEventAddress => {
   return {
-    type: address?.match(/(.*)-/)?.[1]?.trim(),
-    city: address?.match(/\-([^)]+)\//)?.[1]?.trim(),
-    state: address?.match(/\/(.*)/)?.[1]?.trim(),
+    type: address?.match(/(.*)-/)?.[1]?.trim() || undefined,
+    city: address?.match(/\-([^)]+)\//)?.[1]?.trim() || undefined,
+    state: address?.match(/\/(.*)/)?.[1]?.trim() || undefined,
   }
 }
 
